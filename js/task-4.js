09b33c7536e19a604b992c65cdf481c25275e74b
Hümeyra Ayyıldız <humeyrayildiz1996@gmail.com>
2026-03-26 18:39:31 +0300

    add file

--- js/task-4.js
+++ js/task-4.js
@@ -1,48 +1,48 @@
 const getTotalBalanceByGender = (users, gender) => {
-    return users
-        .filter(user => user.gender === gender)
-        .reduce((user, total) => total + user.balance, 0);
-    
+  return users.reduce((total, user) => {
+    if (user.gender === gender) {
+      return total + user.balance;
+    }
+    return total;
+  }, 0);
 };
-    
-
 
 const allUsers = [
-	{
+  {
     name: "Moore Hensley",
     gender: "male",
-    balance: 2811
+    balance: 2811,
   },
   {
     name: "Sharlene Bush",
     gender: "female",
-    balance: 3821
+    balance: 3821,
   },
   {
     name: "Ross Vazquez",
     gender: "male",
-    balance: 3793
+    balance: 3793,
   },
   {
     name: "Elma Head",
     gender: "female",
-    balance: 2278
+    balance: 2278,
   },
   {
     name: "Carey Barr",
     gender: "male",
-    balance: 3951
+    balance: 3951,
   },
   {
     name: "Blackburn Dotson",
     gender: "male",
-    balance: 1498
+    balance: 1498,
   },
   {
     name: "Sheree Anthony",
     gender: "female",
-    balance: 2764
-  }
+    balance: 2764,
+  },
 ];
 
 console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
